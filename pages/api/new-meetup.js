054fb2349b720753f307622c85460043fd4c0778
Ismail Khaled <esmael449@stud.fci-cu.edu.eg>
2022-10-05 20:50:54 +0200

    bug fix

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,9 @@
 import { MongoClient } from "mongodb"
-import { connectionString } from "../../mongoConnect"
 
 const handler = async (req, res)=>{
  if (req.method === 'POST'){
   const data = req.body
-  const client = await MongoClient.connect(connectionString)
+  const client = await MongoClient.connect(process.env.NEXT_PUBLIC_CONNECTION_STRING)
   const db = client.db()
   const meetupsCollection = db.collection('meetups')
   const result = await meetupsCollection.insertOne(data)
